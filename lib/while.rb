d64f6a420652920a878927e728994dec41e4f485
github username <github email address>
2019-07-21 19:03:02 -0400

    Done.

--- lib/while.rb
+++ lib/while.rb
@@ -2,6 +2,20 @@ def using_while
   levitation_force = 6
 	
   #your code here
+  while levitation_force do
+
+    puts "Wingardium Leviosa"
+
+    levitation_force += 1
+
+    if levitation_force >= 10
+
+      break
+    
+    end
+
+  end
+
 end
 
 
